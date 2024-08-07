74906c59eb72d0b80ac403d12d9369e7968545f5
SarEnderTarkanson <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-08-07 12:00:22 +0200

    added the necessary components and pages

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import Homepage from "./pages/HomePage";
-import About from "./pages/About";
+import Homepage from "../pages/HomePage";
+import About from "../pages/About";
 import Reservation from "./pages/BookingPage";
 import Order from "./pages/Order";
 import Login from "./pages/Login";
